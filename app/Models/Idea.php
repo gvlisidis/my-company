8c5afa76f71d5dd1ed8597c19bd1d16378ad1736
George  Vlisidis <gblisidis@gmail.com>
2021-08-30 08:26:52 +0100

    L14 - Add Statuses.

--- app/Models/Idea.php
+++ app/Models/Idea.php
@@ -33,4 +33,9 @@ class Idea extends Model
     {
         return $this->belongsTo(Category::class);
     }
+
+    public function status(): BelongsTo
+    {
+        return $this->belongsTo(Status::class);
+    }
 }
--- tests/Feature/ShowIdeasTest.php
+++ tests/Feature/ShowIdeasTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use App\Models\Category;
 use App\Models\Idea;
+use App\Models\Status;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
@@ -18,15 +19,20 @@ class ShowIdeasTest extends TestCase
         $categoryOne = Category::factory()->create(['name' => 'Category 1']);
         $categoryTwo = Category::factory()->create(['name' => 'Category 2']);
 
+        $statusOpen = Status::factory()->create(['name' => 'Open', 'classes' => 'bg-gray-200']);
+        $statusConsidering = Status::factory()->create(['name' => 'Considering', 'classes' => 'bg-purple text-white']);
+
         $idea1 = Idea::factory()->create([
             'title' => 'My First Idea',
             'category_id' => $categoryOne->id,
+            'status_id' => $statusOpen->id,
             'description' => 'Description of my first Idea',
         ]);
 
         $idea2 = Idea::factory()->create([
             'title' => 'My Second Idea',
             'category_id' => $categoryTwo->id,
+            'status_id' => $statusConsidering->id,
             'description' => 'Description of my second Idea',
         ]);
         $response = $this->get(route('ideas.index'));
@@ -35,18 +41,22 @@ class ShowIdeasTest extends TestCase
         $response->assertSee($idea1->title);
         $response->assertSee($idea1->description);
         $response->assertSee($categoryOne->name);
+        $response->assertSee('bg-gray-200', false);
         $response->assertSee($idea2->title);
         $response->assertSee($idea2->description);
         $response->assertSee($categoryTwo->name);
+        $response->assertSee('bg-purple text-white', false);
     }
 
     /** @test */
     public function single_idea_shows_correctly_on_the_show_page()
     {
         $categoryOne = Category::factory()->create(['name' => 'Category 1']);
+        $statusOpen = Status::factory()->create(['name' => 'Open', 'classes' => 'bg-gray-200']);
         $idea = Idea::factory()->create([
             'title' => 'My First Idea',
             'category_id' => $categoryOne->id,
+            'status_id' => $statusOpen->id,
             'description' => 'Description of my first Idea',
         ]);
 
@@ -56,14 +66,17 @@ class ShowIdeasTest extends TestCase
         $response->assertSee($idea->title);
         $response->assertSee($idea->description);
         $response->assertSee($categoryOne->name);
+        $response->assertSee('bg-gray-200', false);
     }
 
     /** @test */
     public function ideas_pagination_works()
     {
         $categoryOne = Category::factory()->create(['name' => 'Category 1']);
+        $statusOpen = Status::factory()->create(['name' => 'Open', 'classes' => 'bg-gray-200']);
         Idea::factory(Idea::PAGINATION_COUNT + 1)->create([
             'category_id' => $categoryOne->id,
+            'status_id' => $statusOpen->id,
         ]);
 
         $ideaOne = Idea::find(1);
@@ -90,15 +103,18 @@ class ShowIdeasTest extends TestCase
     public function same_idea_title_different_slugs()
     {
         $categoryOne = Category::factory()->create(['name' => 'Category 1']);
+        $statusOpen = Status::factory()->create(['name' => 'Open', 'classes' => 'bg-gray-200']);
         $ideaOne = Idea::factory()->create([
             'title' => 'My First Idea',
             'category_id' => $categoryOne->id,
+            'status_id' => $statusOpen->id,
             'description' => 'Description for my first idea',
         ]);
 
         $ideaTwo = Idea::factory()->create([
             'title' => 'My First Idea',
             'category_id' => $categoryOne->id,
+            'status_id' => $statusOpen->id,
             'description' => 'Another Description for my first idea',
         ]);
 
